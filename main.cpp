4d38237e687f742fc0b147ddfd7bca194c48a384
Isaac <isaacatkinson04@gmail.com>
2026-03-26 02:45:57 +0000

    classes for contacts and types of contacts

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+using namespace std;
+
+class Contact {
+
+    string firstName;
+    string lastName;
+    int phoneNumber;
+};
+
+class workContacts : public Contact {
+
+    string email;
+
+};
+
+class familyFriendsContact : public Contact {
+
+    string address;
+    string dateOfBirth;
+    bool isEmergencyContact = false;
+
+};
+
+int main() {
+
+
+
+}
