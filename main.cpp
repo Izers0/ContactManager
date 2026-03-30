a4c8a0c280cc2ce228e49d8c91cf5756bb7c946a
Isaac <isaacatkinson04@gmail.com>
2026-03-30 05:18:57 +0100

    writeContact function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class Contact {
 
     string firstName;
     string lastName;
-    int phoneNumber;
+    string phoneNumber;
 };
 
 class workContacts : public Contact {
 
     string email;
-
 };
 
 class familyFriendsContact : public Contact {
@@ -19,11 +19,27 @@ class familyFriendsContact : public Contact {
     string address;
     string dateOfBirth;
     bool isEmergencyContact = false;
-
 };
+
+void writeContact();
 
 int main() {
 
+    writeContact();
 
+}
 
+void writeContact() {
+
+    ofstream contactFile;
+
+    contactFile.open("savedContacts.txt", ios::app);
+
+    if (contactFile.is_open()) {
+        cout<< "Contact file successfully opened";
+    } else {
+        cout << "Unable to open file";
+    }
+
+    contactFile << "test";
 }
