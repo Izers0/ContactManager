d8444e9280602298413160876eb51cdff1f05c5e
Isaac <isaacatkinson04@gmail.com>
2026-04-30 04:45:24 +0100

    added setters and getters to the contact class

--- main.cpp
+++ main.cpp
@@ -4,24 +4,69 @@ using namespace std;
 
 class Contact {
 
-    string firstName;
-    string lastName;
-    string phoneNumber;
+    string FirstName;
+    string LastName;
+    string PhoneNumber;
+
+public:
+    void setFirstName(string firstName) {
+
+        FirstName = firstName;
+    }
+    void setLastName(string lastName) {
+
+        LastName = lastName;
+    }
+
+    void setPhoneNumber(string phoneNumber) {
+
+        PhoneNumber = phoneNumber;
+    }
+
+    string getFirstName() {
+
+        return FirstName;
+    }
+    string getLastName() {
+        return LastName;
+    }
+    string getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    void displayInformation() {
+        cout << "First Name: " << FirstName << endl;
+        cout << "Last Name: " << LastName << endl;
+        cout << "Phone Number: " << PhoneNumber << endl;
+    }
 };
 
-class workContacts : public Contact {
+class WorkContacts : public Contact {
 
     string email;
+
+    string getEmail() {
+
+        return email;
+    }
 };
 
-class familyFriendsContact : public Contact {
+class FamilyFriendsContact : public Contact {
 
     string address;
     string dateOfBirth;
     bool isEmergencyContact = false;
+
+    string getAddress() {
+        return address;
+    }
+    string getDateOfBirth() {
+        return dateOfBirth;
+    }
 };
 
 void writeContact();
+void addContact(Contact Firstname);
 
 int main() {
 
@@ -42,4 +87,23 @@ void writeContact() {
     }
 
     contactFile << "test";
+
+    contactFile.close();
+}
+
+void addContact(Contact newContact) {
+
+    string enterFirstName;
+    cout << "Firstname: ";
+    cin >> enterFirstName;
+
+    string enterLastName;
+    cout << "Lastname: ";
+    cin >> enterLastName;
+
+    string enterPhoneNumber;
+    cout << "PhoneNumber: ";
+    cin >> enterPhoneNumber;
+
+    Contact newContact = (enterFirstName, enterLastName, enterPhoneNumber);
 }
